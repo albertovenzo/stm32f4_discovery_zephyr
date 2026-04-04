add186c3e90c5a2adea696fdfe1f291b5cb2f691
AV <alberto.venzo@spiralengineering.it>
2026-04-04 16:36:22 +0200

    use all the other leds

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ static const struct gpio_dt_spec led_orange = GPIO_DT_SPEC_GET(LED1_NODE, gpios)
 static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 static const struct gpio_dt_spec led_blue = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
-int stm32f4_gpio_pin_configure(struct gpio_dt_spec *led)
+int stm32f4_gpio_pin_configure(const struct gpio_dt_spec *led)
 {
 	int ret;
 	if (!gpio_is_ready_dt(led)) {
@@ -38,15 +38,26 @@ int stm32f4_gpio_pin_configure(struct gpio_dt_spec *led)
 int main(void)
 {
 	bool led_state = true;
-	int ret;
+	int ret[4];
+  int i;
 
   stm32f4_gpio_pin_configure(&led_green);
+  stm32f4_gpio_pin_configure(&led_orange);
+  stm32f4_gpio_pin_configure(&led_red);
+  stm32f4_gpio_pin_configure(&led_blue);
 
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led_green);
-		if (ret < 0) {
+
+		ret[0] = gpio_pin_toggle_dt(&led_green);
+		ret[1] = gpio_pin_toggle_dt(&led_orange);
+		ret[2] = gpio_pin_toggle_dt(&led_red);
+		ret[3] = gpio_pin_toggle_dt(&led_blue);
+
+    for(i = 0; i < 4; i++) {
+		  if (ret[i] < 0) {
 			return 0;
-		}
+		  }
+    }
 
 		led_state = !led_state;
 		printf("LED state: %s\n", led_state ? "ON" : "OFF");
